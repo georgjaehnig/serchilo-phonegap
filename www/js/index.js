bc768287078c73f38325c53ab5e25ab8f3f29fb8
Georg Jaehnig <georg@jaehnig.org>
2015-08-22 00:45:51 +0200

    Using JQM - but focus does not work.

--- www/js/index.js
+++ www/js/index.js
@@ -19,7 +19,10 @@
 var app = {
     // Application Constructor
     initialize: function() {
+
         this.bindEvents();
+				alert(1);
+				
     },
     // Bind Event Listeners
     //
@@ -27,23 +30,18 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('load', this.onLoad, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        //app.receivedEvent('deviceready');
+				alert(2);
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+    onDeviceLoad: function() {
+        //app.receivedEvent('deviceready');
+				alert(3);
+    },
 };
